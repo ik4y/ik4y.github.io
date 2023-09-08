81bf344ee6b197201a57667f74c0012a9f0925fb
Imad-K <imadk9700@gmail.com>
2023-09-09 03:24:57 +0500

    Redesigned

---
+++ src/main.js
@@ -0,0 +1,29 @@
+const clientName = document.querySelector(".client_name");
+const clientImage = document.querySelector(".client_image");
+const testimonial = document.querySelector(".testimonials_content");
+
+const testimonials = [
+  {
+    name: "Dylan Lucas",
+    review:
+      "This guy is amazing goes above and beyond to execute any given assignment",
+  },
+
+  {
+    name: "Sahil R (Project Manager at ApanDevs)",
+    review:
+      "I'm delighted to wholeheartdly recommend Imad Khan. His remarkable dedication and expertise as a backend developer were evident throught our collaboration. Imad consistently delivered high-quality work, showing an exceptional ability to solve complex technical challenges. His positive attitude, effective communication, and commitment to excellence make him an invaluable asset to any team. Imad's contribution have significantly enhanced our projects, and I'm confident he will continue to excel in all his future endeavours",
+  },
+];
+
+let i = 0;
+let j = testimonials.length;
+
+let displayTestimonial = () => {
+  setInterval(() => {
+    i = (j + i + 1) % j;
+    testimonial.textContent = testimonials[i].review;
+    clientName.textContent = testimonials[i].name;
+  }, 7000);
+};
+window.onload = displayTestimonial;
